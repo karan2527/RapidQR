ee44aa41f6cd1c7dc082f0fd51d080ae84bc5797
karan2527 <karanjain2527@gmail.com>
2025-02-22 16:35:23 +0530

    main page set-up

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(),
+      title: 'QR code Scanner and Generator',
+      debugShowCheckedModeBanner: false,
+      
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('QR code Scanner and Generator'),
+      ),
+        body : Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  
+                });
+
+
+              }, child: Text('Scan QR code')),
+              SizedBox(height:40 ),
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  
+                });
+              }, child: Text('Generate QR code'),)
+            ],
+      ),
+        
+    )
+      
+    );
+  }
+}
