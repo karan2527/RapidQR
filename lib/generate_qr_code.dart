8d5264b844d12a8b7d03bb18bb041f5681e7dd91
karan2527 <karanjain2527@gmail.com>
2025-02-23 01:06:43 +0530

    dependency issue solved

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'scan_qr_code.dart';
+import 'generate_qr_code.dart';
+
 
 class GenerateQRCode extends StatefulWidget {
   const GenerateQRCode({super.key});
@@ -8,8 +12,36 @@ class GenerateQRCode extends StatefulWidget {
 }
 
 class _GenerateQRCodeState extends State<GenerateQRCode> {
+  TextEditingController urlController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar( title: Text('Generate QR code'),),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if(urlController.text.isNotEmpty)
+              QrImageView(data: urlController.text, size: 200.0),
+                 SizedBox(height: 10),
+                 Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: urlController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your text',
+                      border: OutlineInputBorder(),
+                     
+                    ),
+                    
+                 )
+                 ),
+                    ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -42,8 +42,6 @@ class _HomePageState extends State<HomePage> {
                 setState(() {
                   Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ScanQRCode()));
                 });
-
-
               }, child: Text('Scan QR code')),
               SizedBox(height:40 ),
               ElevatedButton(onPressed: (){
