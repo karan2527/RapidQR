4bbee4d3b0e571130614aa973bb5499f8f694deb
karan2527 <karanjain2527@gmail.com>
2025-02-23 01:16:18 +0530

    button added

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -3,7 +3,6 @@ import 'package:qr_flutter/qr_flutter.dart';
 import 'scan_qr_code.dart';
 import 'generate_qr_code.dart';
 
-
 class GenerateQRCode extends StatefulWidget {
   const GenerateQRCode({super.key});
 
@@ -15,33 +14,35 @@ class _GenerateQRCodeState extends State<GenerateQRCode> {
   TextEditingController urlController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar( title: Text('Generate QR code'),),
+    return Scaffold(
+      appBar: AppBar(title: Text('Generate QR code')),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              if(urlController.text.isNotEmpty)
-              QrImageView(data: urlController.text, size: 200.0),
-                 SizedBox(height: 10),
-                 Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: urlController,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your text',
-                      border: OutlineInputBorder(),
-                     
+              if (urlController.text.isNotEmpty)
+                QrImageView(data: urlController.text, size: 200.0),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: urlController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your text',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                    
-                 )
-                 ),
-                    ],
+                    labelText: 'Enter your text',
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: (){},child: Text('Genrate QR Code'),),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
